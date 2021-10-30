4fbba7f6ee40147f146d2016b4e0f42c1295dee0
woopty WOO <woopty@woo.com>
2021-10-30 17:27:07 -0500

    working on django

---
+++ travel_wishlist/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Place
+from .forms import NewPlaceForm
+
+# Create your views here.
+
+def place_list(request):
+
+    if request.method == 'POST':
+        form = NewPlaceForm(request.POST)
+        place = form.save()
+        if form.is_valid():
+            place.save()
+            return redirect('place_list')
+
+    places = Place.objects.filter(visited=False).order_by('name')
+    new_place_form = NewPlaceForm()
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
+
+def about(request):
+    author = 'Mouhamad'
+    about = 'A website to create a list of places to visit'
+    return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
+
+def places_visited(request):
+    visited = Place.objects.filter(visited=True)
+    return render(request, 'travel_wishlist/visited.html', {'visited': visited})
+
+def place_was_visited(request, place_pk):
+    if request.method == 'POST':
+        # place = Place.objects.get(pk=place_pk)
+        place = get_object_or_404(Place, pk=place_pk)
+        place.visited = True
+        place.save()
+    return redirect('place_list')
