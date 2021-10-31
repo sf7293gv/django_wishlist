0fca5703186a326ac5da26662a5447fb201c9893
woopty WOO <woopty@woo.com>
2021-10-31 16:07:50 -0500

    Working on testing

--- travel_wishlist/tests.py
+++ travel_wishlist/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Place
 
-# Create your tests here.
+class TestHomePage(TestCase):
+    def test_home_page_show_empty_list_message_for_empty_db(self):
+        home_page_url = reverse('place_list')
+        response = self.client.get(home_page_url)
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertContains(response, 'You have no places in your wishlist')
