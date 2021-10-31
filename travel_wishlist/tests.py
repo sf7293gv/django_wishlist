b2536dd3635d56e116df7f3b24bb8fe3babc9a61
woopty WOO <woopty@woo.com>
2021-10-31 16:13:58 -0500

    Added more tests

--- travel_wishlist/tests.py
+++ travel_wishlist/tests.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.test import TestCase
 from django.urls import reverse
 from .models import Place
@@ -8,3 +9,16 @@ class TestHomePage(TestCase):
         response = self.client.get(home_page_url)
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
         self.assertContains(response, 'You have no places in your wishlist')
+
+
+class TestWishList(TestCase):
+    fixtures = ['test_places']
+
+    def test_wishlist_contains_not_visited_places(self):
+        response = self.client.get(reverse('place_list'))
+        self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
+        self.assertContains(response, 'Tokyo')
+        self.assertContains(response, 'New York')
+        self.assertNotContains(response, 'San Francisco')
+        self.assertNotContains(response, 'Moab')      
+
