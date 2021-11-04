e83bada291ebbc20ed239188fae39518e0e441ad
woopty WOO <woopty@woo.com>
2021-11-04 14:19:41 -0500

    added links to visited/wishlist pages

--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -12,5 +12,5 @@ class Place(models.Model):
 
     def __str__(self):
         photo_str = self.photo.url if self.photo else 'no photo'
-        notes_str = self.notes[100:]
+        notes_str = self.notes[100:] if self.notes else 'no notes'
         return f'{self.name} visited? {self.visited} on {self.date_visited}. Notes: {notes_str}. Photo {photo_str}'
--- travel_wishlist/views.py
+++ travel_wishlist/views.py
@@ -1,31 +1,36 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Place
 from .forms import NewPlaceForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
+@login_required
 def place_list(request):
 
     if request.method == 'POST':
         form = NewPlaceForm(request.POST)
-        place = form.save()
+        place = form.save(commit=False)
+        place.user = request.user
         if form.is_valid():
             place.save()
             return redirect('place_list')
 
-    places = Place.objects.filter(visited=False).order_by('name')
+    places = Place.objects.filter(user=request.user).filter(visited=False).order_by('name')
     new_place_form = NewPlaceForm()
     return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
 
+@login_required
 def about(request):
     author = 'Mouhamad'
     about = 'A website to create a list of places to visit'
     return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
 
+@login_required
 def places_visited(request):
     visited = Place.objects.filter(visited=True)
     return render(request, 'travel_wishlist/visited.html', {'visited': visited})
 
+@login_required
 def place_was_visited(request, place_pk):
     if request.method == 'POST':
         # place = Place.objects.get(pk=place_pk)
@@ -33,3 +38,8 @@ def place_was_visited(request, place_pk):
         place.visited = True
         place.save()
     return redirect('place_list')
+
+@login_required
+def place_details(request, place_pk):
+    place = get_object_or_404(Place, pk=place_pk)
+    return render(request, 'travel_wishlist/place_details.html', {'place': place})
