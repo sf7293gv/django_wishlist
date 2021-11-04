72d2fdc448523e23460322d2c8b5c94962661fba
woopty WOO <woopty@woo.com>
2021-11-04 13:13:23 -0500

    Modified models.py (imported user model)

--- travel_wishlist/functional_tests.py
+++ travel_wishlist/functional_tests.py
@@ -1,3 +1,20 @@
-from selenium.webdriver.chrome import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from django.test import LiveServerTestCase
-class
+
+class TitleTest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+    
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+        # super.tearDownClass()
+    
+    def test_title_on_home_page(self):
+        self.selenium.get(self.live_server_url)
+        self.assertIn('Travel Wishlist', self.selenium.title)
