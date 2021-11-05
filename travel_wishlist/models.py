d68e0a0fac73aad641ff79937d8a9b60cd9362a6
woopty WOO <woopty@woo.com>
2021-11-05 17:42:25 -0500

    Added images (option to add them) added tests

--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.files.storage import default_storage
 
 class Place(models.Model):
     user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)
@@ -9,6 +10,23 @@ class Place(models.Model):
     date_visited = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='user_images/', blank=True, null=True)
 
+    def save(self, *args, **kwargs):
+        old_place = Place.objects.filter(pk=self.pk).first()
+        if old_place and old_place.photo:
+            if old_place.photo != self.photo:
+                self.delete_photo(old_place.photo)
+        
+        super().save(*args, **kwargs)
+    
+    def delete_photo(self, photo):
+        if default_storage.exists(photo.name):
+            default_storage.delete(photo.name)
+
+    def delete(self, *args, **kwargs):
+        if self.photo:
+            self.delete_photo(self.photo)
+        
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         photo_str = self.photo.url if self.photo else 'no photo'
