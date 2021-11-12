cef41b812f3eb6de9368eede346b9951c33a7e7a
woopty WOO <woopty@woo.com>
2021-11-11 18:35:46 -0600

    Working on deploying to GCP

--- wishlist/settings.py
+++ wishlist/settings.py
@@ -77,11 +77,17 @@ WSGI_APPLICATION = 'wishlist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'places',
+        'USER': 'Mouhamad',
+        'PASSWORD' : os.getenv('DJANGO_wishlist_USER_PASSWORD'),
+        'HOST': '/cloudsql/django-wishlist-app:us-central1:wishlist-db',
+        'PORT': '5432'
     }
 }
 
+if not os.getenv('GAE_INSTANCE'):
+    DATABASES['default']['HOST'] = '127.0.0.1'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
